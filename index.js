880f914f89a1020c3cb04d6005f6ce6e1bbeeaad
emmaholl97 <emmaholland13@hotmail.com>
2024-03-23 16:52:44 -0400

    updated naming for config fetch

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import { actions as backgroundActions } from "./src/background/actions";
 import { actions as foregroundActions } from "./src/content/actions";
 import { demoFeature as feature } from "./src/content/feature";
 import store from './src/store';
-import featureData from "./feature.config.json";
+import featureConfigData from "./feature.config.json";
 
 const messages = new Messages();
-const featureData = () => featureData;
+const featureData = () => featureConfigData;
 
 export {
     messages,
