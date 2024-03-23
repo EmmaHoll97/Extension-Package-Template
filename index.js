c64490b94fb5207fde07056c576a4761d029385b
emmaholl97 <emmaholland13@hotmail.com>
2024-03-23 16:50:36 -0400

    added feature config template

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ import { actions as backgroundActions } from "./src/background/actions";
 import { actions as foregroundActions } from "./src/content/actions";
 import { demoFeature as feature } from "./src/content/feature";
 import store from './src/store';
+import featureData from "./feature.config.json";
 
 const messages = new Messages();
+const featureData = () => featureData;
 
 export {
     messages,
     backgroundActions,
     foregroundActions,
     feature,
-    store
+    store,
+    featureData
 }
